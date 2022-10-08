404501d4687914e24434fbb6bd6d2fe5372ff1e7
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2022-10-08 15:50:31 +1100

    Create weather app

---
+++ weatherapp/components/Weather/Weather.js
@@ -0,0 +1,14 @@
+import "./Weather.css"
+
+export const Weather = ()=>{
+
+return(
+    <div>
+        <h1>Weather App</h1>
+        <input type='text' placeholder='Search city'></input>
+    
+    </div>
+
+
+)
+}
